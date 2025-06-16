ee3ef2e50ef7140e6aaf7e6baa52faac75e7e8a2
LIUHUAN <liu2012huan@126.com>
2025-06-16 21:09:18 +0800

    [Update] reuse ctx

--- main.go
+++ main.go
@@ -140,7 +140,7 @@ func main() {
 	<-quit
 	log.Println("Shutting down server...")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Printf("Server forced to shutdown: %s\n", err)
